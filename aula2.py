655dc1eda7d3eb0dad59e0b7457ad5f8477e5b56
Marcelo8819 <silva.marcelohg@gmail.com>
2022-10-18 17:33:08 -0300

    Initial commit

---
+++ aula2.py
@@ -0,0 +1,12 @@
+n1 = int(input('Digite o primeiro valor:'))
+n2 = int(input('Digite o segundo valor:'))
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+resto = n1 % n2
+print('A soma é:{}'
+      '\n a subtração é : {}'
+      '\n a multiplicação é : {}'
+      '\n a divisão é: {} '
+      '\n o resto é: {}'.format(soma,subtracao,multiplicacao,divisao,resto))
